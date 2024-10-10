869e9a4d449f4b38f9d9476da7377707ec71d6ab
xixiaolu <2946679089@qq.com>
2024-10-10 16:09:58 +0800

    新增正则3个一个逗号

---
+++ 兄弟组件传参vue/demo/src/views/AddTable.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <el-form ref="form" :model="form" label-width="80px">
+            <el-form-item label="活动时间">
+                <el-date-picker type="date" placeholder="选择日期" v-model="form.date" style="width: 100%;"></el-date-picker>
+            </el-form-item>
+            <el-form-item label="姓名">
+                <el-input v-model="form.name" placeholder="请输入备注"></el-input>
+            </el-form-item>
+            <el-form-item label="地址">
+                <el-input v-model="form.address" placeholder="请输入备注"></el-input>
+            </el-form-item>
+        </el-form>
+        <div class="t-a-c">
+            <el-button type="primary" @click="submit()">新增</el-button>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        components : {
+
+        },
+        data(){
+            return {
+                form : {
+                    date : "",
+                    name : "",
+                    address : "",
+                }
+            }
+        },
+        methods:{
+            submit(){
+                this.$refs.form.validate((valid) => {
+                    if (valid) {
+                        console.log('submit!');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            }
+        },
+    }
+</script>
+<style lang="scss">
+.t-a-c{
+    text-align: center;
+}
+</style>
---
+++ 兄弟组件传参vue/demo/src/views/HomeView.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="home">
+    <div class="t-a-r">
+      <el-button type="primary" @click="addTableClick">新增</el-button>
+    </div>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  // name: 'HomeView',
+  components: {
+    // HelloWorld
+  },
+  data(){
+    return {
+      tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }]
+    }
+  },
+  methods:{
+    addTableClick(){
+      window.open("addTable");
+    }
+  },
+}
+</script>
+<style lang="scss">
+  .t-a-r{
+    text-align: right;
+  }
+</style>
