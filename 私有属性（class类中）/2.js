f278bdc08584b965ceb9950ab5f639439979a9db
xixiaolu <2946679089@qq.com>
2025-01-23 09:30:33 +0800

    增加私有属性

--- 私有属性（class类中）/2.js
+++ 私有属性（class类中）/2.js
@@ -1,11 +1,26 @@
-const fieldMap = new WeakMap();
-export class A {
-    constructor(){
-        fieldMap.set(this,{
-            key : 123,
-        })
+const privateData = new WeakMap();
+
+class Counter {
+    constructor() {
+        privateData.set(this, { count: 0 }); // 私有属性
     }
-    print(){
-        console.log('111',fieldMap.get(this).key);
+
+    increment() {
+        const data = privateData.get(this);
+        data.count++;
+    }
+
+    decrement() {
+        const data = privateData.get(this);
+        data.count--;
+    }
+
+    getCount() {
+        return privateData.get(this).count;
     }
 }
+
+const counter = new Counter();
+counter.increment(); // 增加计数
+counter.decrement(); // 减少计数
+console.log(counter.getCount()); // 输出: 0
