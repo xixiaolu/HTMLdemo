975c570828bacb4f48d082285ff6f43b85595e94
xixiaolu <2946679089@qq.com>
2025-04-07 16:36:35 +0800

    提交了ts文件

--- 使用泛型和keyof约束参数.ts
+++ 使用泛型和keyof约束参数.ts
@@ -17,3 +17,52 @@ function createCard(value : Values,colors : Colors){
 }
 createCard('3','蓝')
 createCard('2','蓝');
+
+
+let s : string = '123';
+let n : number = 123;
+let b : boolean = true;
+function add(a : number,b : number) : number{
+    return a + b;
+}
+
+interface Animal {
+    name : string;
+    eat(foot : string) : void;
+}
+class Dog implements Animal {
+    name : string;
+    constructor(name : string){
+        this.name = name;
+    }
+    eat(foot : string) {
+        console.log('this.name',this.name);
+    }
+}
+
+interface Window {
+    myCustomProp : string;
+}
+
+interface Shape {
+    color : string;
+}
+interface Circle extends Shape {
+    radius : number;
+}
+
+class Cir implements Circle {
+    radius : number;
+    color : string;
+    constructor(radius : number,color : string){
+        this.radius = radius;
+        this.color = color;
+    }
+}
+
+
+type ID = string | number;
+type Coordinates  = [number,number];
+type StringOrNumberArray = Array<string|number>
+
+const str : StringOrNumberArray = [123,'34335',true]
